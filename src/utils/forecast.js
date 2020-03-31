54fc5d9ac008532612f44b0ff1382cc38b87f361
Bibin <bibinjoseph.dev@gmail.com>
2020-03-31 15:02:14 +0530

    Chnages in forecast callback

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,10 +6,15 @@ const forecast = (latitude, longitude, callback) => {
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('unable to forecast weather. Please try again!!', undefined);
-        } else if (body.error || !body.currently) {
+        } else if (body.error || !body.daily || !body.currently) {
             callback('unable to find location. Please try again!!', undefined);
         } else {
-            callback(undefined, 'It is currently ' + body.currently.temperature + ' degrees outside, with ' + body.currently.precipProbability + '% chances of rain.')
+            const dailyObj = body.daily.data[0]
+            callback(undefined, {
+                currentForecast: '<br>It is currently ' + body.currently.temperature + ' degrees outside, with ' + body.currently.precipProbability + '% chances of rain.' + '<br>',
+                dailyForecast: 'High: ' + dailyObj.temperatureHigh + ', Low: ' + dailyObj.temperatureLow
+            })
+
         }
     })
 }
